22e8e5a10f67355375667fc77f625ea41f700fb7
tetoyama <164453224+tetoyama@users.noreply.github.com>
2025-11-17 19:26:28 +0900

    _

--- Source/GameApplication/Engine/Scene/Script/GN31.h
+++ Source/GameApplication/Engine/Scene/Script/GN31.h
@@ -1,5 +1,5 @@
 #pragma once
-#define _CRT_SECURE_NO_WARNINGS // セキュリティ警告
+#define _CRT_SECURE_NO_WARNINGS
 #define _WINSOCK_DEPRECATED_NO_WARNINGS // 古いWinSock関数の警告を無効化
 
 #include <stdio.h>
@@ -47,44 +47,53 @@ private:
 
 	SOCKET listenSocket;
 	char recvBuffer[1024];
-	//SOCKADDR_IN serverAddr;
+	SOCKADDR_IN serverAddr;
 
-	//SOCKADDR_IN clientAddr; // クライアントのアドレス情報
-	//int clientAddrLen = sizeof(clientAddr);
-	//SOCKET clientSocket; // クライアントとの通信に使うソケット
+	SOCKADDR_IN clientAddr; // クライアントのアドレス情報
+	int clientAddrLen = sizeof(clientAddr);
+	SOCKET clientSocket; // クライアントとの通信に使うソケット
+
 
 	void OnStart() override {
-		//m_context->manager->debug->LOG_DEBUG("GN31 Started");
 
-		//m_context->manager->debug->LOG_DEBUG(("ポート番号 : " + std::to_string(port)));
+		char debugString[1024];
+
+		m_context->manager->debug->LOG_DEBUG("GN31 Started");
+
+		m_context->manager->debug->LOG_DEBUG(("ポート番号 : " + std::to_string(port)));
 
 
-		//if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		//	m_context->manager->debug->LOG_ERROR("WSAStartup failed");
-		//	return;
-		//}
-		//m_context->manager->debug->LOG_DEBUG("WSAStartup成功です");
-
-		//hostInfo = (struct hostent*)gethostbyname(hostName);
-		//memcpy(&ipAddr, hostInfo->h_addr_list[0], 4);
-		//strcpy(ipAddress, inet_ntoa(ipAddr));
-		//printf("IPアドレス=%s\n", ipAddress);
-
+		if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+			m_context->manager->debug->LOG_ERROR("WSAStartup failed");
+			return;
+		}
 		// 初期化情報を表示（学習用）
-		//printf("wVersion = %d.%d\n", LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
-		//printf("wHighVersion = %d.%d\n", LOBYTE(wsaData.wHighVersion), HIBYTE(wsaData.wHighVersion));
-		//printf("szDescription = %s\n", wsaData.szDescription);
-		//printf("szSystemStatus = %s\n", wsaData.szSystemStatus);
-		//printf("iMaxSockets= %d\n", wsaData.iMaxSockets);
-		//printf("iMaxUdpDg = %d\n", wsaData.iMaxUdpDg);
-		//printf("WSAStartup成功です\n");
-		//// 自分のホスト名とIPアドレスを取得（確認用）
-		//gethostname(hostName, (int)sizeof(hostName));
-		//printf("\nホスト名=%s\n", hostName);
-		//hostInfo = (struct hostent*)gethostbyname(hostName);
-		//memcpy(&ipAddr, hostInfo->h_addr_list[0], 4);
-		//strcpy(ipAddress, inet_ntoa(ipAddr));
-		//printf("IPアドレス=%s\n", ipAddress);
+		sprintf_s(debugString,"wVersion = %d.%d", LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
+		m_context->manager->debug->LOG_DEBUG(debugString);
+		sprintf_s(debugString,"wHighVersion = %d.%d", LOBYTE(wsaData.wHighVersion), HIBYTE(wsaData.wHighVersion));
+		m_context->manager->debug->LOG_DEBUG(debugString);
+		sprintf_s(debugString,"szDescription = %s", wsaData.szDescription);
+		m_context->manager->debug->LOG_DEBUG(debugString);
+		sprintf_s(debugString,"szSystemStatus = %s", wsaData.szSystemStatus);
+		m_context->manager->debug->LOG_DEBUG(debugString);
+		sprintf_s(debugString,"iMaxSockets= %d", wsaData.iMaxSockets);
+		m_context->manager->debug->LOG_DEBUG(debugString);
+		sprintf_s(debugString,"iMaxUdpDg = %d", wsaData.iMaxUdpDg);
+		m_context->manager->debug->LOG_DEBUG(debugString);
+		m_context->manager->debug->LOG_DEBUG("WSAStartup成功です");
+		
+		// 自分のホスト名とIPアドレスを取得（確認用）
+		gethostname(hostName, (int)sizeof(hostName));
+		hostInfo = (struct hostent*)gethostbyname(hostName);
+		sprintf_s(debugString, "ホスト名=%s", hostName);
+		m_context->manager->debug->LOG_DEBUG(debugString);
+
+
+		memcpy(&ipAddr, hostInfo->h_addr_list[0], 4);
+		strcpy_s(ipAddress, inet_ntoa(ipAddr));
+		sprintf_s(debugString,"IPアドレス=%s", ipAddress);
+		m_context->manager->debug->LOG_DEBUG(debugString);
+
 		//// ③ ソケット作成（TCP通信）
 		//// AF_INET: IPv4, SOCK_STREAM: TCP, 0: プロトコル自動選択
 		//listenSocket = socket(AF_INET, SOCK_STREAM, 0);
