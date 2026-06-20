5b4b0d4e3d540d306bec308ba13c07cdce2186e4
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-21 06:48:13 +0900

    refactor: register transform task from query access

--- Source/GameApplication/Engine/Scene/System/Transform/transformSystem.h
+++ Source/GameApplication/Engine/Scene/System/Transform/transformSystem.h
@@ -9,18 +9,19 @@
 struct SceneManagerContext;
 
 // トランスフォームの描画補助を行うシステム
-class TransformSystem : public ISystem{
+class TransformSystem: public ISystem {
 public:
-
 	const char* GetSystemName() const override{
 		return "TransformSystem";
 	}
-	TransformSystem(SceneManagerContext* context): m_context(context){}
-	~TransformSystem(){}
+
+	explicit TransformSystem(SceneManagerContext* context)
+		: m_context(context){}
 
 	void Initialize() override;
 	void Draw() override;
+	void RegisterTasks(SystemScheduleBuilder& builder) override;
 
 private:
-	SceneManagerContext* m_context;
+	SceneManagerContext* m_context = nullptr;
 };
