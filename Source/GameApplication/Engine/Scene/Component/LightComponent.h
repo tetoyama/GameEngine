1fb963443475c2d461a3c5fdb332da1cf05389a1
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-21 06:36:40 +0900

    refactor: remove light component base inheritance

--- Source/GameApplication/Engine/Scene/Component/LightComponent.h
+++ Source/GameApplication/Engine/Scene/Component/LightComponent.h
@@ -11,7 +11,7 @@
 
 // ライト設定を保持するComponent。
 // YAMLとInspector実装はLightComponentOperationsへ分離する。
-class LightComponent: public IComponent {
+class LightComponent {
 public:
 	LIGHT light{};
 	bool dirty = false;
@@ -25,9 +25,9 @@ public:
 		light.Param = float4(10.0f, 0.0f, 0.0f, DEPTH_BIAS_CONSTANT);
 	}
 
-	YAML::Node encode() override;
-	bool decode(SceneContext* context, const YAML::Node& node) override;
-	void inspector(SceneContext* context) override;
+	YAML::Node encode();
+	bool decode(SceneContext* context, const YAML::Node& node);
+	void inspector(SceneContext* context);
 };
 
 #include "Operations/LightComponentOperations.h"
