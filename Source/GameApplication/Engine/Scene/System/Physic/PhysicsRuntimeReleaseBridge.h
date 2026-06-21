bb81d89d16d02f89818f945f8a59d7a2655553a2
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-22 05:06:28 +0900

    chore: document runtime release bridge removal

--- Source/GameApplication/Engine/Scene/System/Physic/PhysicsRuntimeReleaseBridge.h
+++ Source/GameApplication/Engine/Scene/System/Physic/PhysicsRuntimeReleaseBridge.h
@@ -2,3 +2,4 @@
 
 // Retired: Collider runtime resources are released by resolving
 // PhysicSystem from SceneContext before storage removal.
+// No mutable process-global release hook remains here.
