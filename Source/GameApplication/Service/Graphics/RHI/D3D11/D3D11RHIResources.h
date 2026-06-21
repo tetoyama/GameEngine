112a4f0a9d285e0a92a14966c2039abb5f8b11c3
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-21 17:10:28 +0900

    rhi: add D3D11 view and sampler resources

--- Source/GameApplication/Service/Graphics/RHI/D3D11/D3D11RHIResources.h
+++ Source/GameApplication/Service/Graphics/RHI/D3D11/D3D11RHIResources.h
@@ -12,16 +12,34 @@ namespace RHI {
 struct D3D11BufferResource {
 	BufferDesc desc;
 	ResourceState state = ResourceState::Common;
+	uint32_t viewCount = 0;
 	Microsoft::WRL::ComPtr<ID3D11Buffer> object;
 };
 
 struct D3D11TextureResource {
 	TextureDesc desc;
 	std::vector<ResourceState> subresourceStates;
+	uint32_t viewCount = 0;
 	Microsoft::WRL::ComPtr<ID3D11Texture2D> object;
+};
+
+struct D3D11BufferViewResource {
+	BufferViewDesc desc;
 	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderView;
+	Microsoft::WRL::ComPtr<ID3D11UnorderedAccessView> unorderedAccessView;
+};
+
+struct D3D11TextureViewResource {
+	TextureViewDesc desc;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderView;
+	Microsoft::WRL::ComPtr<ID3D11UnorderedAccessView> unorderedAccessView;
 	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderView;
 	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> depthView;
+};
+
+struct D3D11SamplerResource {
+	SamplerDesc desc;
+	Microsoft::WRL::ComPtr<ID3D11SamplerState> object;
 };
 
 struct D3D11ShaderResource {
