14859e9ab07a51c2dbd9c0ade0064682a9fb0261
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-27 02:22:44 +0900

    Add template animation task registrar

---
+++ Source/GameApplication/Engine/Scene/System/Render/Animation/RenderSystemAnimationTaskRegistrar.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <utility>
+
+#include "Graphics/graphicsContext.h"
+#include "Interface/ISystem.h"
+#include "Resources/Data/modelData.h"
+#include "Resources/resourceService.h"
+#include "Scene/Component/modelRendererComponent.h"
+#include "Scene/sceneManager.h"
+
+namespace RenderSystemAnimationTaskRegistrar {
+
+template<typename RenderSystemT>
+void Register(
+	RenderSystemT& system,
+	SystemScheduleBuilder& builder,
+	float deltaTime
+){
+	SystemAccess poseAccess;
+	poseAccess
+		.WriteComponent<ModelRendererComponent>()
+		.ReadResource<SceneManager>()
+		.ReadResource<ModelData>();
+
+	builder.AddTask(
+		"RenderSystem.Animation.Pose.Calculate",
+		SystemTaskDomain::Editor,
+		SystemPhase::Earliest,
+		0,
+		std::move(poseAccess),
+		ThreadAffinity::AnyWorker,
+		[&system](const SystemTaskContext&){
+			system.CalculateAnimationPoses();
+		}
+	);
+
+	SystemAccess uploadAccess;
+	uploadAccess
+		.WriteComponent<ModelRendererComponent>()
+		.ReadResource<SceneManager>()
+		.WriteResource<ResourceService>()
+		.WriteResource<ModelData>()
+		.WriteResource<GraphicsContext>();
+
+	builder.AddTask(
+		"RenderSystem.Animation.Upload",
+		SystemTaskDomain::Editor,
+		SystemPhase::Early,
+		0,
+		std::move(uploadAccess),
+		ThreadAffinity::MainThread,
+		[&system, deltaTime](const SystemTaskContext&){
+			system.UploadAnimationPoses(deltaTime);
+		}
+	);
+}
+
+} // namespace RenderSystemAnimationTaskRegistrar
