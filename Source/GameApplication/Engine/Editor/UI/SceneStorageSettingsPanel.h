711de18064260ab11858e3cb30b7c94e0163dfe0
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-26 15:49:28 +0900

    Add scene settings panel

---
+++ Source/GameApplication/Engine/Editor/UI/SceneStorageSettingsPanel.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Editor/editorService.h"
+#include "Editor/UI/MenuBar.h"
+#include "Editor/UI/SceneStorageSettingsView.h"
+
+class SceneStorageSettingsPanel final : public IEditorUI {
+public:
+	void Initialize(EditorService* editor) override { m_editor = editor; }
+	void Draw(const EditorDrawContext) override {
+		MenuBar* menu = m_editor ? m_editor->GetUI<MenuBar>() : nullptr;
+		bool* open = menu ? &menu->showSceneSettings : nullptr;
+		SceneStorageSettingsView::Draw(m_editor, open);
+	}
+	void Finalize() override {}
+private:
+	EditorService* m_editor = nullptr;
+};
