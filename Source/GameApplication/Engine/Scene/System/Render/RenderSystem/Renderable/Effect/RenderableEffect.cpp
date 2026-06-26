c7a1c26f0347c0bcea90cdd2c24fa71f461a4f35
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-26 16:21:30 +0900

    Clean Effect packet rendering

--- Source/GameApplication/Engine/Scene/System/Render/RenderSystem/Renderable/Effect/RenderableEffect.cpp
+++ Source/GameApplication/Engine/Scene/System/Render/RenderSystem/Renderable/Effect/RenderableEffect.cpp
@@ -6,30 +6,24 @@
 #include "RenderableEffect.h"
 #include "Backends/convertMatrix.h"
 #include "Graphics/graphicsContext.h"
-#include "System/Render/RenderSystem/renderSystem.h"
-#include "Registry/componentRegistry.h"
 #include "scene.h"
 #include "sceneManager.h"
 #include "../../RenderPass/RenderPassContext.h"
-#include "../../RenderPacket/RenderPacketTransformDX11.h"
 #include <Component/EffectComponent.h>
 
 void RenderableEffect::Execute(const RenderPassContext& ctx, const RenderPacket& packet){
 	SceneContext* sceneContext = packet.bindings.sceneContext;
-	const Entity& entity = packet.entity;
 	if(!sceneContext) return;
 
 	EffectComponent* effect = packet.bindings.effect;
-	if(!effect){
-		return;
-	}
-	Effekseer::Matrix44 effekseerProjectionMatrix = ConvertXMMATRIXToMatrix44(ctx.projectionMatrix);
-	Effekseer::Matrix44 effekseerViewMatrix = ConvertXMMATRIXToMatrix44(ctx.viewMatrix);
+	if(!effect) return;
 
-	m_context->graphics->GetEffectRenderer()->SetProjectionMatrix(effekseerProjectionMatrix);
-	m_context->graphics->GetEffectRenderer()->SetCameraMatrix(effekseerViewMatrix);
-
-	m_context->graphics->GetEffectRenderer()->BeginRendering();
+	Effekseer::Matrix44 projection = ConvertXMMATRIXToMatrix44(ctx.projectionMatrix);
+	Effekseer::Matrix44 view = ConvertXMMATRIXToMatrix44(ctx.viewMatrix);
+	auto* renderer = m_context->graphics->GetEffectRenderer();
+	renderer->SetProjectionMatrix(projection);
+	renderer->SetCameraMatrix(view);
+	renderer->BeginRendering();
 	m_context->graphics->GetEffectManager()->DrawHandle(effect->m_Handle);
-	m_context->graphics->GetEffectRenderer()->EndRendering();
+	renderer->EndRendering();
 }
