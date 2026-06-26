b569a185bfd98c050cb208913e8c24dc1420ff45
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-26 16:30:44 +0900

    Clean Particle render header

--- Source/GameApplication/Engine/Scene/System/Render/RenderSystem/Renderable/Particle/RenderableParticle.h
+++ Source/GameApplication/Engine/Scene/System/Render/RenderSystem/Renderable/Particle/RenderableParticle.h
@@ -5,7 +5,6 @@
 // =======================================================================
 #pragma once
 #include "../IRenderable.h"
-class ComponentRegistry;
 
 struct RenderPassContext;
 struct SceneManagerContext;
