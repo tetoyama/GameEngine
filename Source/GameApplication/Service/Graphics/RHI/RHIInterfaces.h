dfcde4debc35d1224648b5e55e1938294d4b0e3f
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-28 11:19:57 +0900

    Add indexed instanced draw to RHI contract

--- Source/GameApplication/Service/Graphics/RHI/RHIInterfaces.h
+++ Source/GameApplication/Service/Graphics/RHI/RHIInterfaces.h
@@ -31,6 +31,13 @@ public:
 	virtual bool UpdateBuffer(BufferHandle buffer, std::span<const std::byte> data, uint32_t destinationOffset = 0) = 0;
 	virtual void Draw(uint32_t vertexCount, uint32_t firstVertex = 0) = 0;
 	virtual void DrawIndexed(uint32_t indexCount, uint32_t firstIndex = 0, int32_t vertexOffset = 0) = 0;
+	virtual void DrawIndexedInstanced(
+		uint32_t indexCountPerInstance,
+		uint32_t instanceCount,
+		uint32_t firstIndex = 0,
+		int32_t vertexOffset = 0,
+		uint32_t firstInstance = 0
+	) = 0;
 	virtual void Dispatch(uint32_t groupCountX, uint32_t groupCountY, uint32_t groupCountZ) = 0;
 };
 
