c93194d6d8400a4d4b167707586ec1f8ea979189
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-28 14:12:23 +0900

    Import legacy D3D11 buffers into RHI handles

--- Source/GameApplication/Service/Graphics/RHI/D3D11/D3D11RHIBufferRuntime.h
+++ Source/GameApplication/Service/Graphics/RHI/D3D11/D3D11RHIBufferRuntime.h
@@ -54,4 +54,97 @@ inline BufferHandle D3D11RHIDevice::CreateBuffer(
 	return m_buffers.Create(std::move(resource));
 }
 
+inline BufferHandle D3D11RHIDevice::ImportNativeBuffer(
+	ID3D11Buffer* nativeBuffer,
+	uint32_t stride,
+	ResourceState initialState,
+	const char* debugName
+){
+	if(!m_device || !nativeBuffer || initialState == ResourceState::Undefined){
+		return {};
+	}
+
+	D3D11_BUFFER_DESC nativeDesc{};
+	nativeBuffer->GetDesc(&nativeDesc);
+	if(nativeDesc.ByteWidth == 0) return {};
+
+	BufferDesc desc;
+	desc.byteSize = nativeDesc.ByteWidth;
+	desc.stride = stride;
+	switch(nativeDesc.Usage){
+		case D3D11_USAGE_IMMUTABLE:
+			desc.usage = ResourceUsage::Immutable;
+			break;
+		case D3D11_USAGE_DYNAMIC:
+			desc.usage = ResourceUsage::Dynamic;
+			break;
+		case D3D11_USAGE_STAGING:
+			desc.usage = ResourceUsage::Staging;
+			break;
+		default:
+			desc.usage = ResourceUsage::Default;
+			break;
+	}
+
+	if((nativeDesc.BindFlags & D3D11_BIND_VERTEX_BUFFER) != 0){
+		desc.bindFlags |= BufferBindFlags::Vertex;
+	}
+	if((nativeDesc.BindFlags & D3D11_BIND_INDEX_BUFFER) != 0){
+		desc.bindFlags |= BufferBindFlags::Index;
+	}
+	if((nativeDesc.BindFlags & D3D11_BIND_CONSTANT_BUFFER) != 0){
+		desc.bindFlags |= BufferBindFlags::Constant;
+	}
+	if((nativeDesc.BindFlags & D3D11_BIND_SHADER_RESOURCE) != 0){
+		desc.bindFlags |= BufferBindFlags::ShaderResource;
+	}
+	if((nativeDesc.BindFlags & D3D11_BIND_UNORDERED_ACCESS) != 0){
+		desc.bindFlags |= BufferBindFlags::UnorderedAccess;
+	}
+	if((nativeDesc.MiscFlags & D3D11_RESOURCE_MISC_DRAWINDIRECT_ARGS) != 0){
+		desc.bindFlags |= BufferBindFlags::IndirectArguments;
+	}
+
+	if((nativeDesc.CPUAccessFlags & D3D11_CPU_ACCESS_READ) != 0){
+		desc.cpuAccess |= CpuAccessFlags::Read;
+	}
+	if((nativeDesc.CPUAccessFlags & D3D11_CPU_ACCESS_WRITE) != 0){
+		desc.cpuAccess |= CpuAccessFlags::Write;
+	}
+
+	desc.structured =
+		(nativeDesc.MiscFlags & D3D11_RESOURCE_MISC_BUFFER_STRUCTURED) != 0;
+	desc.raw =
+		(nativeDesc.MiscFlags & D3D11_RESOURCE_MISC_BUFFER_ALLOW_RAW_VIEWS) != 0;
+	if(desc.structured){
+		if(nativeDesc.StructureByteStride == 0) return {};
+		if(desc.stride == 0){
+			desc.stride = nativeDesc.StructureByteStride;
+		}else if(desc.stride != nativeDesc.StructureByteStride){
+			return {};
+		}
+	}
+
+	if(initialState == ResourceState::VertexBuffer &&
+		!HasAnyFlag(desc.bindFlags, BufferBindFlags::Vertex)){
+		return {};
+	}
+	if(initialState == ResourceState::IndexBuffer &&
+		!HasAnyFlag(desc.bindFlags, BufferBindFlags::Index)){
+		return {};
+	}
+	if(HasAnyFlag(desc.bindFlags, BufferBindFlags::Vertex) && desc.stride == 0){
+		return {};
+	}
+
+	desc.initialState = initialState;
+	if(debugName) desc.debugName = debugName;
+
+	D3D11BufferResource resource;
+	resource.desc = std::move(desc);
+	resource.state = initialState;
+	resource.object = nativeBuffer;
+	return m_buffers.Create(std::move(resource));
+}
+
 } // namespace RHI
