994d536ccbd5fbee21cc6a865e89d898e824fe86
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-28 11:34:34 +0900

    Test RHI service device ownership

--- Tests/RHISmokeTest.cpp
+++ Tests/RHISmokeTest.cpp
@@ -1,25 +1,37 @@
 #include <cassert>
+
 #include "Service/Graphics/RHI/Null/NullRHIBackend.h"
+#include "Service/Graphics/RHI/RHIService.h"
 
 int main(){
-	RHI::BackendRegistry registry;
-	assert(RHI::RegisterNullRHIBackend(registry));
-	auto backend = registry.Create(RHI::BackendType::Null);
-	assert(backend);
+	RHI::RenderHardwareInterfaceService service;
+	assert(RHI::RegisterNullRHIBackend(service.GetRegistry()));
+	assert(service.SelectBackend(RHI::BackendType::Null));
+	assert(service.GetBackend());
 
 	RHI::DeviceCreateDesc desc;
 	desc.swapChain.width = 640;
 	desc.swapChain.height = 360;
-	auto device = backend->CreateDevice(desc);
+	auto device = service.GetBackend()->CreateDevice(desc);
 	assert(device);
-	assert(device->GetQueue(RHI::CommandQueueType::Graphics));
+	assert(service.AdoptDevice(std::move(device)));
+	assert(service.GetDevice());
+	assert(service.GetDevice()->GetQueue(RHI::CommandQueueType::Graphics));
 
 	RHI::CommandListCreateDesc commandDesc;
 	commandDesc.queueType = RHI::CommandQueueType::Graphics;
-	auto commandList = device->CreateCommandList(commandDesc);
+	auto commandList = service.GetDevice()->CreateCommandList(commandDesc);
 	assert(commandList);
 	commandList->Begin();
 	assert(!commandList->DrawIndexedInstanced(36, 4));
 	commandList->End();
+
+	auto released = service.ReleaseDevice();
+	assert(released);
+	assert(!service.GetDevice());
+	assert(service.AdoptDevice(std::move(released)));
+	service.Shutdown();
+	assert(!service.GetDevice());
+	assert(!service.GetBackend());
 	return 0;
 }
