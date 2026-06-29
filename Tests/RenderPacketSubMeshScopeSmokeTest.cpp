b5ca97009a72181572da7db7917889420a78bd2d
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-29 09:21:50 +0900

    Test shared model submesh range resolution

--- Tests/RenderPacketSubMeshScopeSmokeTest.cpp
+++ Tests/RenderPacketSubMeshScopeSmokeTest.cpp
@@ -3,11 +3,27 @@
 #include <vector>
 
 #include "Engine/Scene/System/Render/RenderSystem/RenderPacket/RenderPacket.h"
+#include "Engine/Scene/System/Render/RenderSystem/RenderPacket/RenderPacketModelSubMeshSelection.h"
 
 int main(){
 	RenderPacket legacyPacket;
 	assert(legacyPacket.TargetsAllSubMeshes());
 	assert(!legacyPacket.TargetsSubMesh(0));
+
+	const RenderPacketModelSubMeshRange allRange =
+		RenderPacketModelSubMeshSelection::ResolveRange(legacyPacket, 3);
+	assert(allRange.IsValid());
+	assert(!allRange.IsSingleSubMesh());
+	assert(allRange.firstIndex == 0);
+	assert(allRange.count == 3);
+
+	std::uint32_t resolvedIndex = 99;
+	assert(!RenderPacketModelSubMeshSelection::ResolveSingleIndex(
+		legacyPacket,
+		3,
+		resolvedIndex
+	));
+	assert(resolvedIndex == 0);
 
 	RenderPacket firstSubMesh;
 	firstSubMesh.sceneContextID = 1;
@@ -25,11 +41,30 @@ int main(){
 	firstSubMesh.stableSequence = 2;
 	assert(!firstSubMesh.TargetsAllSubMeshes());
 	assert(firstSubMesh.TargetsSubMesh(0));
+	assert(RenderPacketModelSubMeshSelection::ResolveSingleIndex(
+		firstSubMesh,
+		3,
+		resolvedIndex
+	));
+	assert(resolvedIndex == 0);
 
 	RenderPacket secondSubMesh = firstSubMesh;
 	secondSubMesh.subMeshIndex = 1;
 	secondSubMesh.stableSequence = 1;
 	assert(secondSubMesh.TargetsSubMesh(1));
+	assert(RenderPacketModelSubMeshSelection::ResolveSingleIndex(
+		secondSubMesh,
+		3,
+		resolvedIndex
+	));
+	assert(resolvedIndex == 1);
+
+	RenderPacket invalidSubMesh = firstSubMesh;
+	invalidSubMesh.subMeshIndex = 3;
+	assert(!RenderPacketModelSubMeshSelection::ResolveRange(
+		invalidSubMesh,
+		3
+	).IsValid());
 
 	RenderPacket allSubMeshes = firstSubMesh;
 	allSubMeshes.subMeshIndex = RenderPacketAllSubMeshes;
