111bc721e6260c3d4eb05b76f331e928de47f956
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-30 19:51:25 +0900

    Split directional and perspective shadow rasterizers

--- Source/GameApplication/Engine/Scene/System/Render/RenderSystem/RenderPass/ShadowMap/ShadowMapPass.h
+++ Source/GameApplication/Engine/Scene/System/Render/RenderSystem/RenderPass/ShadowMap/ShadowMapPass.h
@@ -26,7 +26,8 @@ public:
 	std::vector<IRenderable*> renderables;
 	RenderTarget* shadowRenderTarget = nullptr;
 	ID3D11SamplerState* shadowSampler = nullptr;
-	ID3D11RasterizerState* depthClampRS = nullptr; // DepthClipEnable=FALSE: フラスタム外 shadow caster のクリッピングを防ぐ
+	ID3D11RasterizerState* depthClampRS = nullptr;
+	ID3D11RasterizerState* perspectiveShadowRS = nullptr;
 
 private:
 	void RenderShadowScene(const RenderPassContext& ctx);
