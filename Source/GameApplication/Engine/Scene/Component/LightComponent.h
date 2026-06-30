1207d591ee1a037712a1d12d31c2ecf391f3d22c
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-30 14:28:32 +0900

    Default new lights to no shadow

--- Source/GameApplication/Engine/Scene/Component/LightComponent.h
+++ Source/GameApplication/Engine/Scene/Component/LightComponent.h
@@ -6,24 +6,16 @@
 #pragma once
 
 #include "Interface/IComponent.h"
-#include "Shader/Common.hlsl"
-#include "Shader/CommonDefine.h"
+#include "LightComponentDefaults.h"
 
 // ライト設定を保持するComponent。
 // YAMLとInspector実装はLightComponentOperationsへ分離する。
 class LightComponent {
 public:
-	LIGHT light{};
+	LIGHT light = LightComponentDefaults::Create();
 	bool dirty = false;
 
-	LightComponent(){
-		light.Enable = true;
-		light.LightType = LIGHT_TYPE_POINT;
-		light.CastShadow = true;
-		light.Ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
-		light.Diffuse = float4(1.0f, 1.0f, 1.0f, 1.0f);
-		light.Param = float4(10.0f, 0.0f, 0.0f, DEPTH_BIAS_CONSTANT);
-	}
+	LightComponent() = default;
 
 	YAML::Node encode();
 	bool decode(SceneContext* context, const YAML::Node& node);
