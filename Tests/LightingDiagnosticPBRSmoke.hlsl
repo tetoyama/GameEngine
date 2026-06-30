3d52bbf4de954363d76c674e626f9cdb806d106e
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-30 11:28:40 +0900

    Add lighting diagnostic PBR shader smoke

---
+++ Tests/LightingDiagnosticPBRSmoke.hlsl
@@ -0,0 +1,12 @@
+#include "../Source/Shader/commonDefine.h"
+#include "../Source/Shader/common.hlsl"
+#include "../Source/Shader/Material/MaterialDefine.hlsli"
+#include "../Source/Shader/Material/DeferredFunc.hlsli"
+#include "../Source/Shader/Material/MaterialFunc.hlsli"
+#include "../Source/Shader/Material/PBRShader.hlsli"
+
+float4 main(PS_IN input) : SV_Target
+{
+    MaterialInput materialInput = GetMaterialInput(input);
+    return ShadeMaterial_PBR(materialInput);
+}
