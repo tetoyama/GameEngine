b100981825b69bee0b0be9938d824c6456b01704
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-06-30 14:36:27 +0900

    Remove Windows bool macro dependency

--- Source/GameApplication/Engine/Scene/Component/LightComponentDefaults.h
+++ Source/GameApplication/Engine/Scene/Component/LightComponentDefaults.h
@@ -7,9 +7,9 @@ namespace LightComponentDefaults {
 
 inline LIGHT Create() noexcept {
 	LIGHT light{};
-	light.Enable = TRUE;
+	light.Enable = 1;
 	light.LightType = LIGHT_TYPE_POINT;
-	light.CastShadow = FALSE;
+	light.CastShadow = 0;
 	light.Ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
 	light.Diffuse = float4(1.0f, 1.0f, 1.0f, 1.0f);
 	light.Param = float4(10.0f, 0.0f, 0.0f, DEPTH_BIAS_CONSTANT);
