5e82501cc882a89949b1f4b221852306886f2f71
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-07-12 06:01:19 +0900

    Own camera post effect runtime in pass

--- Source/GameApplication/Engine/Scene/System/Render/RenderSystem/RenderPass/PostEffect/PostEffectPass.h
+++ Source/GameApplication/Engine/Scene/System/Render/RenderSystem/RenderPass/PostEffect/PostEffectPass.h
@@ -4,8 +4,11 @@
 // 
 // =======================================================================
 #pragma once
-#include "../IRenderPass.h"
+
 #include <d3d11.h>
+
+#include "../IRenderPass.h"
+#include "CameraPostEffectRuntimeStorage.h"
 
 class GBufferPass;
 
@@ -16,13 +19,18 @@ public:
 	void Finalize() override;
 	void Execute(const RenderPassContext& ctx) override;
 
-	void SetInputs(ID3D11ShaderResourceView* initialSRV,ID3D11RenderTargetView** initialRTV,  GBufferPass* gBufferPass);
+	void SetInputs(
+		ID3D11ShaderResourceView* initialSRV,
+		ID3D11RenderTargetView** initialRTV,
+		GBufferPass* gBufferPass
+	);
 
 	ID3D11ShaderResourceView* resultSrv = nullptr;
 	ID3D11RenderTargetView** resultRtv = nullptr;
 
 private:
-	ID3D11ShaderResourceView* m_initialSRV  = nullptr;
+	ID3D11ShaderResourceView* m_initialSRV = nullptr;
 	ID3D11RenderTargetView** m_initialRTV = nullptr;
-	GBufferPass*              m_gBufferPass = nullptr;
+	GBufferPass* m_gBufferPass = nullptr;
+	CameraPostEffectRuntimeStorage m_cameraRuntime;
 };
