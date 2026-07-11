ddad68f72ba7f614fd1133125e44977961f71ce6
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-07-12 06:23:22 +0900

    Keep camera previews valid across camera switches

--- Source/GameApplication/Engine/Scene/System/Render/RenderSystem/RenderPass/PostEffect/PostEffectPass.h
+++ Source/GameApplication/Engine/Scene/System/Render/RenderSystem/RenderPass/PostEffect/PostEffectPass.h
@@ -10,7 +10,6 @@
 #include "../IRenderPass.h"
 #include "CameraPostEffectRuntimeStorage.h"
 
-class CameraComponent;
 class GBufferPass;
 
 // ポストエフェクト処理パス
@@ -30,11 +29,8 @@ public:
 	ID3D11RenderTargetView** resultRtv = nullptr;
 
 private:
-	void ClearPreviewHandles() noexcept;
-
 	ID3D11ShaderResourceView* m_initialSRV = nullptr;
 	ID3D11RenderTargetView** m_initialRTV = nullptr;
 	GBufferPass* m_gBufferPass = nullptr;
 	CameraPostEffectRuntimeStorage m_cameraRuntime;
-	CameraComponent* m_previewCamera = nullptr;
 };
