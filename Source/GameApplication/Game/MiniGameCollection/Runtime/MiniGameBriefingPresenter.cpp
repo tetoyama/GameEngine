8a45d53c70d917ab50767fb616d53b77aa7578b6
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-07-14 21:21:37 +0900

    Compile-check briefing presenter in GameEngine

---
+++ Source/GameApplication/Game/MiniGameCollection/Runtime/MiniGameBriefingPresenter.cpp
@@ -0,0 +1,7 @@
+#include "Game/MiniGameCollection/Runtime/MiniGameBriefingPresenter.h"
+
+namespace MiniGameCollection::Runtime {
+
+static_assert(sizeof(MiniGameBriefingPresenter) == 1);
+
+} // namespace MiniGameCollection::Runtime
